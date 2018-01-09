197a58e86e740a09f9babf38dd03a5d1cfa2daa1
Liudmyla Iefremova <liudmyla.iefremova@gmail.com>
2018-01-09 17:49:07 +0300

    fix auth bug

--- server/app.js
+++ server/app.js
@@ -1,93 +1,85 @@
 const express = require('express');
-const history = require('express-history-api-fallback');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-//const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const User = require('./models/user');
+const config = require('./config');
 const { Strategy, ExtractJwt } = require('passport-jwt');
-const config = require('./config'); //JWT secret
+
 const mongoose = require('mongoose');
-const dbName = 'blog';
-mongoose.connect(`mongodb://localhost/${dbName}`, { useMongoClient: true });
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
+mongoose.connect('mongodb://localhost/blog', { useMongoClient: true });
 
 const app = express();
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
 
 passport.initialize();
-
+// Create the strategy for JWT
 const strategy = new Strategy({
-  secretOrKey: config.jwtSecret,
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
-},
-(payload, done) => {
-  User.findById(payload.id)
-  .then(user => {
-    if(user) {
-      done(null, user);
-    } else {
-      done(new Error('User not found'))
-    }
-  });
-});
-
+		// this is a config we pass to the strategy
+	  // it needs to secret to decrypt the payload of the
+	  // token.
+	  secretOrKey: config.jwtSecret,
+    // This options tells the strategy to extract the token
+    // from the header of the request
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+	},
+	(payload, done) => {
+		// payload is the object we encrypted at the route /api/token
+    // We get the user id, make sure the user exist by looking it up
+    User.findById(payload.id).then(user => {
+    	if(user){
+    		// make the user accessible in req.user
+    		done(null, user);
+    	} else {
+    		done(new Error("User not found"));
+    	}
+    });
+	}
+);
+//tell passport to use it
 passport.use(strategy);
 
 
 const index = require('./routes/index');
-const users = require('./routes/users');
+const authRoutes = require('./routes/auth');
 
 app.use('/', index);
-app.use('/api/users', users);
+app.use('/api', authRoutes);
 
-// protecting the route
 app.get(
 	"/api/secret",
-	// this is protecting the route and giving us access to req.user
-	passport.authenticate('jwt', config.jwtSession),
-	(req,res) => {
-	// send the user his own information
-	res.json(req.user);
-	}
-);
+	// this is protecting the route and giving us access to
+  // req.user
+  passport.authenticate('jwt', config.jwtSession),
+  (req,res) => {
+  	// send the user his own information
+  	res.json(req.user);
+  }
+	);
 
-app.use('/', express.static(path.join(__dirname, '../client/dist')))
-app.use(history('index.html', { root: path.join(__dirname, '../client/dist') }))
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.render('error');
+  // return the error message only in development mode
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err.message : {}
+  });
 });
 
 module.exports = app;
--- server/models/user.js
+++ server/models/user.js
@@ -1,20 +1,12 @@
 const mongoose = require('mongoose');
-const timestamps = require('mongoose-timestamp');
-const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    username: { 
-        type: String, 
-        required: true,
-    },
-    email: { 
-        type: String, 
-        required: true,
-    },
+	// the username and password are added by passportLocalMongoose
+	name: String
 });
 
 userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(timestamps);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema);
